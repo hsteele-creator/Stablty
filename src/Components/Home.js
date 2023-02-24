537409b51729a81a10f4ac5ccb5f1e6df638680f
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-02-24 14:15:13 -0500

    working on strengthen your mind

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Overview from "./Overview";
 import Solution from "./Solution";
+import Opportunity from "./Oportunity";
 
 const Home = () => {
     return (
         <>
         <Overview />
         <Solution />
+        <Opportunity />
         </>
     ) 
 }
---
+++ src/Components/Oportunity.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Overview.css";
+import "./Oportunity.css"
+
+const Opportunity = () => {
+  return (
+    <>
+    <h1 id="opportunity-title">Strengthen Your Mind</h1>
+    <div id="opportunity-container">
+      <div className="opportunity-box">
+        Stablty helps you control your life by providing key skills to help you
+        navigate <span>hightened emotional states</span> like stress, anger,
+        dissapointment
+      </div>
+      <div className="opportunity-box" >
+        Stablty helps you control your life by providing key skills to help you
+        navigate <span>hightened emotional states</span> like stress, anger,
+        dissapointment
+      </div>
+      <div className="opportunity-box">
+        Stablty helps you control your life by providing key skills to help you
+        navigate <span>hightened emotional states</span> like stress, anger,
+        dissapointment
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Opportunity;
