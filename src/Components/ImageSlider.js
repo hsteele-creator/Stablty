94a463a37bf671a21805c645c27c720eb131d177
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-03-02 17:59:55 -0500

    made slider for overview need to work on autoplay next

---
+++ src/Components/ImageSlider.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./ImageSlider.css";
+import { useState } from "react";
+
+const ImageSLider = ({ slides }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const slideStyles = {
+    width: "100%",
+    height: "100%",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundImage: `url(${slides[currentIndex].url})`,
+  };
+
+  const sliderStyles = {
+    height: "100%",
+    position: "relative",
+  };
+
+  const leftArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translater(0, -50%)",
+    left: "32px",
+    fontSixe: "45px",
+    color: "white",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const rightArrowStyles = {
+    position: "absolute",
+    top: "50%",
+    transform: "translater(0, -50%)",
+    right: "32px",
+    fontSixe: "45px",
+    color: "white",
+    zIndex: 1,
+    cursor: "pointer",
+  };
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const dotContainersStyles = {
+    display : "flex",
+    justifyContent : "center"
+  }
+
+  const dotStyles = {
+    margin : "-50px 3px",
+    cursor : "pointer",
+    fontSize : "20px"
+  }
+
+  return (
+    <div style={sliderStyles}>
+      <div style={leftArrowStyles} onClick={goToPrevious}>
+        &#8678;
+      </div>
+      <div style={rightArrowStyles} onClick={goToNext}>
+        &#8680;
+      </div>
+      <div style={slideStyles}></div>
+
+      <div style={dotContainersStyles}>
+        {slides.map((slide, i) => {
+          return <div style={dotStyles} key={i} onClick={() => setCurrentIndex(i)}>●</div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ImageSLider;
--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -4,34 +4,45 @@ import { NavLink } from "react-router-dom";
 import { Button } from "@mui/material";
 import bluebg from "./bluebg.jpg";
 import mountain from "./mountain.jpg";
-import BlueWhite from "./bluewhite.jpg"
+import BlueWhite from "./bluewhite.jpg";
+import GradientBG from "./gradient-bg.png";
+import ImageSLider from "./ImageSlider";
+
+const slides = [{ url: mountain }, { url: bluebg }, { url: BlueWhite }];
 
 const Overview = () => {
   return (
     <div
       id="overview-container"
       style={{
-        backgroundImage: `url(${BlueWhite})`,
+        backgroundImage: `url(${GradientBG})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         backgroundSize: "cover",
       }}
     >
-      <h1>Overview</h1>
-      <h6>Who we are</h6>
-      <p>
-        We help people create mental stability, and develop a mindful practice.
-      </p>
-      <p>
-        Diverse, varied, scientifically proven exercises engage users to develop
-        healthy habits and positively impact their mental health
-      </p>
+      <div id="text-container">
+        <h1>Overview</h1>
+        <h6>Who we are</h6>
+        <p>
+          We help people create mental stability, and develop a mindful
+          practice.
+        </p>
+        <p>
+          Diverse, varied, scientifically proven exercises engage users to
+          develop healthy habits and positively impact their mental health
+        </p>
 
-      <Button id="help-btn">
-        <NavLink to="/help" id="help-link">
-          We can Help
-        </NavLink>
-      </Button>
+        <Button id="help-btn">
+          <NavLink to="/help" id="help-link">
+            We can Help
+          </NavLink>
+        </Button>
+      </div>
+
+      <div id="image-slider-container">
+        <ImageSLider slides={slides} />
+      </div>
     </div>
   );
 };
